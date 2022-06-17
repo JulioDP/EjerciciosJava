7bdf8813d3b52e57477c6464d9df338f10b340c7
Julio Dominguez <chiapasjeremai@gmail.com>
2022-06-17 10:07:56 -0500

    Ejercicio Concatenacion

--- src/ejerciciosOpenbootcamp/EjercicioTemaTres.java
+++ src/ejerciciosOpenbootcamp/EjercicioTemaTres.java
@@ -1,2 +1,16 @@
-package ejerciciosOpenbootcamp;public class EjercicioTemaTres {
+package ejerciciosOpenbootcamp;
+
+public class EjercicioTemaTres {
+
+    private static String [] lista = {"Juan", "Camilo", "Angel", "Pablo", "Tomas", "Timoteo", "Julio", "Jeremias"};
+    public static void main(String[] args) {
+        System.out.println(Concatenar());
+    }
+    private static String Concatenar(){
+        String unir = "";
+        for (String nombres: lista) {
+            unir += nombres +"\n";
+        }
+        return unir;
+    }
 }
